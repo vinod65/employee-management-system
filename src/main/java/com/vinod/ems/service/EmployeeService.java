23b491b39d91ec1dfcefbd34503f321c4d8fa149
Vinod T R <125855964+vinod65@users.noreply.github.com>
2026-07-07 19:49:26 +0530

    Implement average salary calculation in EmployeeService
    Added method to calculate the average salary of employees.

--- src/main/java/com/vinod/ems/service/EmployeeService.java
+++ src/main/java/com/vinod/ems/service/EmployeeService.java
@@ -14,23 +14,47 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    public List<EmployeEntity> getAllEmployees() {
-        return employeeRepository.findAll();
-    }
-
+    // Save Employee
     public EmployeEntity saveEmployee(EmployeEntity employee) {
         return employeeRepository.save(employee);
     }
 
+    // Get All Employees
+    public List<EmployeEntity> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    // Get Employee By Id
     public EmployeEntity getEmployeeById(Long id) {
         return employeeRepository.findById(id).orElse(null);
     }
 
+    // Update Employee
     public EmployeEntity updateEmployee(EmployeEntity employee) {
         return employeeRepository.save(employee);
     }
 
+    // Delete Employee
     public void deleteEmployee(Long id) {
         employeeRepository.deleteById(id);
     }
+
+   
+    public double getAverageSalary() {
+
+        List<EmployeEntity> employees = employeeRepository.findAll();
+
+        if (employees.isEmpty()) {
+            return 0;
+        }
+
+        double totalSalary = 0;
+
+        for (EmployeEntity employee : employees) {
+            totalSalary += employee.getSalary();
+        }
+
+        return totalSalary / employees.size();
+    }
+
 }
